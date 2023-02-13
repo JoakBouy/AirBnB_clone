adc68cbf57281a0f076d2e9d143ff85a07fd958a
JoakBouy <B.joak@alustudent.com>
2023-02-13 23:15:27 +0300

    updated to link BaseModel to FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,10 @@
 import uuid
+import models.engine.file_storage
 from datetime import datetime
 
 class BaseModel:
+    storage = models.engine.file_storage.FileStorage()
+
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
@@ -15,12 +18,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+            storage.new(self)
     
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         my_dict = self.__dict__.copy()
